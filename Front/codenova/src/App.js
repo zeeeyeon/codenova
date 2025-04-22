328add26e3312f8db76f137555450f139b979b28
Yujiin <jiin4083@naver.com>
2025-04-22 17:03:31 +0900

    S12P31E103-39 [FE] feat: 프론트 프로젝트 초기세팅

---
+++ Front/codenova/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>TEST test 글꼴</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Front/codenova/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+      "./src/**/*.{html,js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+            sans: ['NeoDGM', 'sans-serif'],
+          },
+      },
+    },
+    plugins: [],
+  }
+  
