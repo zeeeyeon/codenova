dc24331907038903d3ec6ec54c8fc4523ac13f16
jeong young han <daum4572@naver.com>
2025-04-24 23:51:14 +0900

    S12P31E103-60 [BE] feat: 소켓 기본 설정

---
+++ Backend/src/main/java/kr/codenova/backend/global/config/socket/SocketConfig.java
@@ -0,0 +1,50 @@
+package kr.codenova.backend.global.config.socket;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.Transport;
+import kr.codenova.backend.multi.socket.SocketEventHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class SocketConfig {
+
+    private final List<SocketEventHandler> socketEventHandlers;
+
+    @Value("${socketio.server.hostname}")
+    private String hostname;
+
+    @Value("${socketio.server.port}")
+    private int port;
+
+
+    /**
+     * Tomcat 서버와 별도로 돌아가는 netty 서버를 생성
+     */
+    @Bean
+    public SocketIOServer socketIOServer() {
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setHostname("0.0.0.0");
+        config.setPort(9092);
+        config.setOrigin("*");
+        config.setAuthorizationListener(handshakeData -> true);  // ✅ 무조건 허용
+        config.setTransports(Transport.WEBSOCKET); // Only WebSocket
+
+        config.setBossThreads(1);
+        config.setWorkerThreads(8);
+
+
+        SocketIOServer server = new SocketIOServer(config);
+
+        // ✅ 모든 핸들러 리스너 등록
+        for (SocketEventHandler handler : socketEventHandlers) {
+            handler.registerListeners(server);
+        }
+
+        return server;
+    }
+}
