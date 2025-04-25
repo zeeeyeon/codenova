6f41fb50ff480bfec9c3fadd550bd00f02c41b1b
이지연 <jiyeon_0342@naver.com>
2025-04-25 12:39:19 +0900

    toCodeList -> toLanguageList 메서드 이름 변경

--- Backend/src/main/java/kr/codenova/backend/single/controller/SingleController.java
+++ Backend/src/main/java/kr/codenova/backend/single/controller/SingleController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static kr.codenova.backend.global.response.ResponseCode.GET_CS_CATEGORIES_SUCCESS;
 import static kr.codenova.backend.global.response.ResponseCode.GET_LANGUAGE_CATEGORIES_SUCCESS;
 
 @RestController
