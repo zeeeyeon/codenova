8a4c74a0faecf9ee2ee5d7c98b1e643a31c2a2fc
Yujiin <jiin4083@naver.com>
2025-04-25 11:38:19 +0900

    S12P31E103-64 [FE] feat: main, header 경로 설정

--- Front/codenova/src/components/common/Header.jsx
+++ Front/codenova/src/components/common/Header.jsx
@@ -2,15 +2,18 @@ import logo from "../../assets/images/codenova_logo.png"; // 🚀 왼쪽 로켓 
 import mypageIcon from "../../assets/images/mypage_icon.png";
 import rankingIcon from "../../assets/images/ranking_icon.png";
 import helpIcon from "../../assets/images/help_icon.png";
-
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+
+  const navigate = useNavigate()
+
   return (
     <header className="w-full flex justify-between items-center px-3 py-3">
       {/* 왼쪽 로고 */}
-      <img src={logo} alt="Logo" className="w-40" />
+      <img src={logo} alt="Logo" className="w-40 cursor-pointer hover:translate-y-[2px] hover:scale-[0.98] active:scale-[0.95]" onClick={() => navigate("/main")} />
 
       {/* 오른쪽 아이콘들 */}
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-2">
         <img
           src={mypageIcon}
           alt="My Page"
