0b4dc9af4af6108bc6e86d406db232c8c906e165
Yujiin <jiin4083@naver.com>
2025-04-26 21:36:43 +0900

    S12P31E103-64 [FE] UI: tuto, header UI

--- Front/codenova/src/routes/MeteoRoutes.jsx
+++ Front/codenova/src/routes/MeteoRoutes.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import MeteoLandingPage from "../pages/meteo/MeteoLandingPage";
-
+import MeteoGamePage from "../pages/meteo/MeteoGamePage";
 const MeteoRoutes = () => {
   return (
     <div className="w-full h-full">
       <Routes>
         <Route path="landing" element={<MeteoLandingPage />} />
+        <Route path="game" element={<MeteoGamePage />} />
       </Routes>
     </div>
   );
