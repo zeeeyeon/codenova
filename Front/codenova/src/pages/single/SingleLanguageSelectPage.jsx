8b0b418a052e075037391742e3b698b878fdb5ce
송동현 <dustkscjswo@naver.com>
2025-04-27 23:22:36 +0900

    Fe/ui/single modal

--- Front/codenova/src/pages/single/SingleLanguageSelectPage.jsx
+++ Front/codenova/src/pages/single/SingleLanguageSelectPage.jsx
@@ -1,11 +1,11 @@
-import backgroundImg from '../../assets/images/main_background.avif'
+import backgroundImg from '../../assets/images/single_background.jpg'
 import javaBtn from '../../assets/images/java_button.png'
 import pythonBtn from '../../assets/images/python_button.png'
 import sqlBtn from '../../assets/images/SQL_button.png'
 import cBtn from '../../assets/images/C_button.png'
 import csBtn from '../../assets/images/CS_button.png'
-import box from '../../assets/images/board1.jpg'
 import cancelBtn from '../../assets/images/cancel_btn.png'
+import BoardContainer from '../../components/common/single/BoardContainer'
 
 
 const SingleLanguageSelectPage = () => {
@@ -15,16 +15,7 @@ const SingleLanguageSelectPage = () => {
       style={{backgroundImage: `url(${backgroundImg})`}}
     >
 
-      <div className='relative w-[50vw] h-[35vw] max-w-5xl py-12 px-6 flex flex-col items-center gap-6 rounded-2xl'
-        style={{
-          backgroundImage: `url(${box})`,
-          backgroundSize: '100% 100%',
-          backgroundRepeat: 'no-repeat'
-        }}
-      >
-        {/* 선택 박스 배경 이미지 */}
-        {/* <img src={box} alt="언어선택박스" className='w-full h-full object-cover rounded-2xl'/> */}
-
+      <BoardContainer>
         {/* 타이틀 텍스트 */}
         <div className="absolute top-[1%] left-1/2 -translate-x-1/2 font-bold text-4xl drop-shadow-md z-10"
              style={{color: '#1C1C1C'}}
@@ -45,8 +36,9 @@ const SingleLanguageSelectPage = () => {
         <div className='relative w-[16vw] max-w-[300px] mt-10'>
           <img src={cancelBtn} alt="취소" className='w-full h-full rounded-3xl cursor-pointer transition-all duration-200 hover:brightness-110 hover:translate-y-[2px] hover:scale-[0.97] active:scale-[0.94]'/>
         </div>
+      </BoardContainer>
 
-      </div>
+
     </div>
   )  
 };
