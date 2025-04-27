370bce9265bb9005d881a3190dd3dc4c15047f9d
skdud5126 <skdud5126@naver.com>
2025-04-27 17:45:32 +0900

    S12P31E103-75 [FE] design : 멀티모드 방 대기 목록 리스트 UI 구현완료(with 더미데이터)

--- Front/codenova/src/components/multi/RoomList.jsx
+++ Front/codenova/src/components/multi/RoomList.jsx
@@ -5,17 +5,23 @@ import RoomItem from "../multi/RoomItem";
 const dummyRooms = [
     { id : 1, title: "찡이방 한판뜨자!", currentPeople: 2, standardPeople: 4, language : "Python", isPublic: true, status: "waiting"},
     { id : 2, title: "아무나 들어오셈!", currentPeople: 1, standardPeople: 2, language : "Java", isPublic: false, status: "waiting"},
-    { id : 3, title: "아무나 들어오셈!", currentPeople: 2, standardPeople: 2, language : "JavaScript", isPublic: false, status: "playing"},
+    { id : 3, title: "다함께 코드노바!", currentPeople: 2, standardPeople: 2, language : "JavaScript", isPublic: false, status: "playing"},
+    { id : 4, title: "아무나 들어오셈!", currentPeople: 2, standardPeople: 2, language : "JavaScript", isPublic: false, status: "playing"},
+    { id : 4, title: "아무나 들어오셈!", currentPeople: 2, standardPeople: 2, language : "JavaScript", isPublic: false, status: "playing"},
+    { id : 4, title: "아무나 들어오셈!", currentPeople: 2, standardPeople: 2, language : "JavaScript", isPublic: false, status: "playing"},
+    { id : 4, title: "아무나 들어오셈!", currentPeople: 2, standardPeople: 2, language : "JavaScript", isPublic: false, status: "playing"},
 ]
 
 
 const RoomList = () => {
     return (
-        <div className="flex flex-col gap-4 items-center mt-4">
-        {dummyRooms.map((room) => (
-        <RoomItem key={room.id} room={room} />
-      ))}
-    </div>
+        <div className="grid grid-cols-3 gap-x-11 gap-y-5 items-start justify-items-center mt-6 px-3
+ max-h-[90%] overflow-y-auto custom-scrollbar">
+  
+  {dummyRooms.map((room) => (
+    <RoomItem key={room.id} room={room} />
+  ))}
+</div>
     );
 };
 
