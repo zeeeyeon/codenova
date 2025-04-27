c8b54dc834edd45f8e507fb760fecf2a1a9b0ae4
ddoriboy <ddoriboy@naver.com>
2025-04-27 23:33:51 +0900

    S12P31E103-81 [BE] feat: 실시간으로 입력한 텍스트 전송 이벤트

--- Backend/src/main/java/kr/codenova/backend/meteor/MeteorEventHandler.java
+++ Backend/src/main/java/kr/codenova/backend/meteor/MeteorEventHandler.java
@@ -46,6 +46,8 @@ public class MeteorEventHandler {
         server.addEventListener("startGame", StartGameRequest.class, (client, data, ack) -> handleGameStart(client, data));
         // 대기방 나가기 이벤트
         server.addEventListener("exitRoom", ExitRoomRequest.class, (client, data, ack) -> handleExitRoom(client, data));
+        // 실시간 입력 텍스트 전송 이벤트
+        server.addEventListener("inputText", InputTextRequest.class, (client, data, ack) -> handleInputText(client, data));
     }
 
     private void handleCreateRoom(SocketIOClient client, CreateRoomRequest data) {
@@ -260,6 +262,31 @@ public class MeteorEventHandler {
 
 
 
+    }
+
+    private void handleInputText(SocketIOClient client, InputTextRequest data) {
+        String text = data.getText();
+        String nickname = data.getNickname();
+
+        if (text == null || text.isBlank()) {
+            log.warn("handleInputText: text를 입력해주세요 ");
+            return;
+        }
+        InputTextResponse response = new InputTextResponse(text, nickname);
+        // 클라이언트가 속한 방 ID 찾기
+        //    getAllRooms()에는 세션ID(=client.getSessionId().toString())도 포함되므로, 이를 제외하고 방 ID를 꺼냄.
+        String roomId = client.getAllRooms().stream()
+                .filter(r -> !r.equals(client.getSessionId().toString()))
+                .findFirst()
+                .orElse(null);
+
+        if (roomId != null) {
+            // 해당 방에 이벤트 브로드캐스트
+            server.getRoomOperations(roomId)
+                    .sendEvent("textInput", client, response);
+        } else {
+            log.warn("handleInputText: 클라이언트가 어느 방에도 속해있지 않습니다. sessionId={}", client.getSessionId());
+        }
     }
 
     public ConnectListener listenConnected() {
