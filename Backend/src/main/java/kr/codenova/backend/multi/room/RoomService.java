0163acea73204ec0ed122e396e7695307f8ee9ef
jeong young han <daum4572@naver.com>
2025-04-28 11:58:41 +0900

    S12P31E103-40 [BE] refactor: Room Service와 Handler 기능 분리

--- Backend/src/main/java/kr/codenova/backend/multi/room/RoomService.java
+++ Backend/src/main/java/kr/codenova/backend/multi/room/RoomService.java
@@ -1,32 +1,34 @@
 package kr.codenova.backend.multi.room;
 
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
 import kr.codenova.backend.multi.dto.request.CreateRoomRequest;
 import kr.codenova.backend.multi.dto.request.JoinRoomRequest;
+import kr.codenova.backend.multi.dto.request.LeaveRoomRequest;
+import kr.codenova.backend.multi.dto.response.RoomListResponse;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.List;
 
 public interface RoomService {
 
     // 방 만들기
-    public Room createRoom(CreateRoomRequest request);
+    public void createRoom(CreateRoomRequest request, AckRequest ackSender);
+
+    // 방 목록 조회
+    public List<RoomListResponse> getRoomList();
 
     // 방 조회
     public Room getRoom(String roomId);
 
     // 방에 접속하기
-    public Room joinRoom(JoinRoomRequest request);
-
-    // 빈 방 확인하기
-    public Boolean isRoomEmpty(String roomId);
+    public void joinRoom(JoinRoomRequest request, SocketIOClient client, AckRequest ackSender);
 
     // 전체 방 목록 조회
     public Collection<Room> getAllRooms();
 
     // 방 나가기 처리
-    public void leaveRoom(String roomId);
+    public void leaveRoom(LeaveRoomRequest request, SocketIOClient client);
 
     // 룸 코드 생성
     public String generatedRoomCode();
--- Backend/src/main/java/kr/codenova/backend/multi/room/RoomSocketHandler.java
+++ Backend/src/main/java/kr/codenova/backend/multi/room/RoomSocketHandler.java
@@ -23,7 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RoomSocketHandler implements SocketEventHandler {
 
-    private final RoomServiceImpl roomService;
+    private final RoomService roomService;
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Override
@@ -31,70 +31,39 @@ public class RoomSocketHandler implements SocketEventHandler {
 
         // 방 목록 조회
         server.addEventListener("room_list", Void.class, (client, request, ackSender) -> {
-            Collection<Room> rooms = roomService.getAllRooms();
-            List<RoomListResponse> responseList = rooms.stream()
-                            .map(room -> new RoomListResponse(
-                                    room.getRoomId(),
-                                    room.getRoomTitle(),
-                                    room.getCurrentCount(),
-                                    room.getMaxCount(),
-                                    room.getLanguage(),
-                                    room.getIsLocked(),
-                                    room.getIsStarted()))
-                                    .toList();
-            ackSender.sendAckData(responseList); // 요청한 클라이언트에게 응답
+            try {
+                ackSender.sendAckData(roomService.getRoomList());
+            } catch (Exception e) {
+                client.sendEvent("error", new SocketErrorResponse("방 목록 조회 실패"));
+            }
         });
 
         // 방 생성
         server.addEventListener("create_room", CreateRoomRequest.class, (client, request, ackSender) -> {
-
-            Room newRoom = roomService.createRoom(request);
-
-            log.info("방 생성 : " + newRoom.toString());
-
-            RoomUpdateBroadcast broadcast = RoomUpdateBroadcast.from(newRoom);
-            CreateRoomResponse response = new CreateRoomResponse(newRoom);
-            // 응답으로 roomId 전달
-            ackSender.sendAckData(response);
-            log.info("방 생성 : " + request.getTitle());
-            server.getBroadcastOperations().sendEvent("room_update", broadcast);
+            try {
+                roomService.createRoom(request, ackSender);
+            } catch (Exception e) {
+                client.sendEvent("error", new SocketErrorResponse("방 생성 실패"));
+            }
         });
 
         // 방 입장
         server.addEventListener("join_room", JoinRoomRequest.class, (client, request, ackSender) -> {
             try {
-                Room room = roomService.joinRoom(request);
-
-                client.joinRoom(room.getRoomId());
-
-                ackSender.sendAckData("joined");
-
-                RoomUpdateBroadcast broadcast = RoomUpdateBroadcast.from(room);
-                server.getBroadcastOperations().sendEvent("room_updated", broadcast);
-            } catch (RoomNotFoundException | RoomFullException e) {
-                // 소켓 클라이언트에 에러 전송
-                client.sendEvent("error", new SocketErrorResponse(e.getMessage()));
+                roomService.joinRoom(request, client, ackSender);
             } catch (Exception e) {
-                client.sendEvent("error", new SocketErrorResponse("알 수 없는 오류가 발생했습니다."));
+                client.sendEvent("error", new SocketErrorResponse("방 입장 실패"));
             }
         });
 
         // 방 퇴장
         server.addEventListener("leave_room", LeaveRoomRequest.class, (client, request, ackSender) -> {
-            roomService.leaveRoom(request.getRoomId());
-            client.leaveRoom(request.getRoomId());
-
-            if (roomService.isRoomEmpty(request.getRoomId())) {
-                server.getBroadcastOperations().sendEvent("room_removed", request.getRoomId());
-            } else {
-                Room room = roomService.getRoom(request.getRoomId());
-                RoomUpdateBroadcast updated = RoomUpdateBroadcast.from(room);
-                server.getBroadcastOperations().sendEvent("room_update", updated);
+            try {
+                roomService.leaveRoom(request, client);
+            } catch (Exception e) {
+                client.sendEvent("error", new SocketErrorResponse("방 퇴장 실패"));
             }
         });
-
-
-
     }
 
 }
