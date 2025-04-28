8f0d4fa6f3f2d0d42fa0c63c6ce81df2ae01bb09
skdud5126 <skdud5126@naver.com>
2025-04-28 22:42:05 +0900

    S12P31E103-79 [FE] design : 멀티모드 비공개 룸코드 일치했을때만  입장 가능 구현 완료

--- Front/codenova/src/components/multi/modal/EnterRoomModal.jsx
+++ Front/codenova/src/components/multi/modal/EnterRoomModal.jsx
@@ -1,10 +1,33 @@
 import React, { useState } from "react";
 import modalBg from "../../../assets/images/board3.png";
-import cancleBtn from "../../../assets/images/cancle_btn_2.png";  // ✅ 취소 버튼
-import goRoomBtn from "../../../assets/images/multi_go_room_btn.png"; // ✅ 입장 버튼
+import cancleBtn from "../../../assets/images/cancle_btn_2.png";  //  취소 버튼
+import goRoomBtn from "../../../assets/images/multi_go_room_btn.png"; //  입장 버튼
 
-const EnterRoomModal = ({ isPrivate, roomTitle, roomLanguage, currentPeople,standardPeople,onClose, onEnter }) => {
+const EnterRoomModal = ({ 
+    isPrivate, 
+    roomTitle, 
+    roomLanguage, 
+    currentPeople,
+    standardPeople,
+    onClose, 
+    onEnter, 
+    correctRoomCode }) => {
   const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState(false);
+  const [shake, setShake] = useState(false);
+
+    const handleEnter = () => {
+    if (roomCode === correctRoomCode) {
+        onEnter(roomCode); // ✅ 성공!
+    } else {
+        setError(true);    // ❌ 에러 표시
+        setShake(true);    // ❌ 흔들림 추가
+        setTimeout(() => {
+        setShake(false);
+        }, 500); // 흔들림은 0.5초만
+    }
+    };
+
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
@@ -30,17 +53,22 @@ const EnterRoomModal = ({ isPrivate, roomTitle, roomLanguage, currentPeople,stan
               <input
                 type="text"
                 value={roomCode}
-                onChange={(e) => setRoomCode(e.target.value)}
+                onChange={(e) => {
+                    setRoomCode(e.target.value);
+                    if (error) setError(false); // 수정 시 에러 해제
+                  }}
                 onKeyDown={(e) => {
-                  if (e.key === "Enter") onEnter(roomCode);
+                  if (e.key === "Enter")  handleEnter();
                 }}
                 onFocus={(e) => (e.target.placeholder = "")} // 포커스 되면 placeholder 삭제
                 onBlur={(e) => (e.target.placeholder = "방 코드 입력")} // 포커스 풀리면 다시 복구
-                className="border rounded px-4 py-2 text-black w-[250px] mb-4 mx-auto text-center"
-                placeholder="방 코드 입력"
+                className={`border rounded px-4 py-2 text-black text-xl w-[240px] mb-4 mx-auto text-center 
+                    ${error ? 'border-red-500 border-4' : ''} 
+                    ${shake ? 'animate-shake' : ''}`}   // 에러시 테두리 빨간색 + 흔들림
+                  placeholder="방 코드 입력"
               />
 
-                <div className="flex gap-6 mt-2">
+                <div className="flex gap-12 mt-2">
                 {/* 취소 버튼 */}
                 <button
                   onClick={onClose}
@@ -51,7 +79,7 @@ const EnterRoomModal = ({ isPrivate, roomTitle, roomLanguage, currentPeople,stan
 
                 {/* 입장 버튼 */}
                 <button
-                  onClick={() => onEnter()}
+                  onClick={handleEnter}
                   className="w-[105px] h-[40px] hover:brightness-110 hover:scale-95 transition"
                 >
                   <img src={goRoomBtn} alt="입장" className="w-full h-full object-contain" />
@@ -67,7 +95,7 @@ const EnterRoomModal = ({ isPrivate, roomTitle, roomLanguage, currentPeople,stan
               <p className="text-xl text-gray-300 mb-1">언어: {roomLanguage}</p>
               <p className="text-xl text-gray-300 mb-2">인원: {currentPeople}/{standardPeople}명</p>
 
-              <div className="flex justify-center gap-8 mt-4">
+              <div className="flex justify-center gap-12 mt-4">
                 {/* 취소 버튼 */}
                 <button
                   onClick={onClose}
