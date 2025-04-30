e090ea8cb1ad9197c2f57619a9c78e4513f8b7f6
ddoriboy <ddoriboy@naver.com>
2025-04-30 12:05:23 +0900

    S12P31E103-104 [BE] fix: 단어 낙하 이벤트 브로드캐스트 수정

--- Backend/src/main/java/kr/codenova/backend/meteor/dto/response/FallingWordResponse.java
+++ Backend/src/main/java/kr/codenova/backend/meteor/dto/response/FallingWordResponse.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,4 +14,5 @@ import lombok.Setter;
 public class FallingWordResponse {
     private String word;
     private long fallDuration;
+    private String timestamp;
 }
