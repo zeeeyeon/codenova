670b63d59f56bda58b033b3e51eeb9d549a035c4
jeong young han <daum4572@naver.com>
2025-05-02 17:02:56 +0900

    S12P31E103-60 [BE] refactor: 방 입장 시, 응답 브로드캐스트 수정

--- Backend/src/main/java/kr/codenova/backend/multi/dto/broadcast/JoinRoomBroadcast.java
+++ Backend/src/main/java/kr/codenova/backend/multi/dto/broadcast/JoinRoomBroadcast.java
@@ -1,13 +1,42 @@
 package kr.codenova.backend.multi.dto.broadcast;
 
+import kr.codenova.backend.multi.room.Room;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class JoinRoomBroadcast {
-    private String nickname;
-    private Integer currentCount;
+    private String roomId;
+    private String roomCode;
+    private List<UserStatus> status;
+
+    @Data
+    public static class UserStatus {
+        private String nickname;
+        private Boolean isReady;
+
+        public UserStatus(String nickname, Boolean isReady) {
+            this.nickname = nickname;
+            this.isReady = isReady;
+        }
+    }
+
+    public JoinRoomBroadcast(Room room) {
+        this.roomId = room.getRoomId();
+        this.roomCode = room.getIsLocked() ? room.getRoomCode() : null; // ✅ 조건부 포함
+        List<UserStatus> status = new ArrayList<>();
+        Map<String, Boolean> userReadyStatus = room.getUserReadyStatus();
+        for (Map.Entry<String, Boolean> entry : userReadyStatus.entrySet()) {
+            status.add(new UserStatus(entry.getKey(), entry.getValue()));
+        }
+        this.status = status;
+    }
 }
