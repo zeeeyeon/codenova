1e392e105b43359f0133ffc60559e67c6ce8fa69
zeeeyeon <zeeyeonleee@gmail.com>
2025-05-03 22:02:31 +0900

    S12P31E103-142 [BE] feat: 언어 카테고리별 랭킹(로그인시, 내 최고 타수도 같이 보여주기)

---
+++ Backend/src/main/java/kr/codenova/backend/single/service/impl/RedisRankingService.java
@@ -0,0 +1,55 @@
+package kr.codenova.backend.single.service.impl;
+
+import kr.codenova.backend.common.enums.Language;
+import kr.codenova.backend.single.dto.response.RankingResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class RedisRankingService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public void saveTypingSpeed(Language language, String nickname, double speed) {
+        String key = getRankingKey(language);
+        redisTemplate.opsForZSet().add(key, nickname, speed);
+    }
+
+    public RankingResponse getRanking(Language language, String nickname) {
+        List<RankingResponse.RankingEntry> top10 = getTop10(language);
+        RankingResponse.MyRank myRank = (nickname != null) ? getMyRank(language, nickname) : null;
+        return new RankingResponse(top10, myRank);
+    }
+
+    private List<RankingResponse.RankingEntry> getTop10(Language language) {
+        String key = getRankingKey(language);
+        Set<ZSetOperations.TypedTuple<String>> top10 = redisTemplate.opsForZSet().reverseRangeWithScores(key, 0, 9);
+
+        if (top10 == null) return List.of();
+
+        return top10.stream()
+                .map(entry -> new RankingResponse.RankingEntry(entry.getValue(), entry.getScore()))
+                .toList();
+    }
+
+    private RankingResponse.MyRank getMyRank(Language language, String nickname) {
+        String key = getRankingKey(language);
+
+        Long rank = redisTemplate.opsForZSet().reverseRank(key, nickname);
+        Double score = redisTemplate.opsForZSet().score(key, nickname);
+
+        if (rank == null || score == null) return null;
+
+        return new RankingResponse.MyRank(rank.intValue() + 1, score);
+    }
+
+    private String getRankingKey(Language language) {
+        return "ranking:" + language.name().toLowerCase();
+    }
+}
