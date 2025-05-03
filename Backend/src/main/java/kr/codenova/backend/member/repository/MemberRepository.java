7fa957c1187c3c7bbf0861d0b4fd30858acbbcde
이지연 <jiyeon_0342@naver.com>
2025-05-04 03:07:06 +0900

    S12P31E103-143 [BE] refactor: CustomMemberDetail, @Param("id") 설정

--- Backend/src/main/java/kr/codenova/backend/member/repository/MemberRepository.java
+++ Backend/src/main/java/kr/codenova/backend/member/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import kr.codenova.backend.member.entity.Member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,6 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, Integer> {
 
     @Query("SELECT m FROM Member m WHERE m.id = :id")
     @EntityGraph(attributePaths = {"nickname"})
-    Optional<Member> findByIdColumn(String id);
+    Optional<Member> findByIdColumn(@Param("id")String id);
     Optional<Member> findByNickname(String nickname);
 }
