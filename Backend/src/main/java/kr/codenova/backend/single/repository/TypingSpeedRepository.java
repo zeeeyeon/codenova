2408c8f61bcf971b186c80faf992539f4d0e109f
zeeeyeon <zeeyeonleee@gmail.com>
2025-05-03 17:38:23 +0900

    S12P31E103-140 [BE] feat: 랭킹 저장시 사용하는 TypingSpeed, UniqueConstraint, Index 설정

--- Backend/src/main/java/kr/codenova/backend/single/repository/TypingSpeedRepository.java
+++ Backend/src/main/java/kr/codenova/backend/single/repository/TypingSpeedRepository.java
@@ -3,10 +3,13 @@ package kr.codenova.backend.single.repository;
 import kr.codenova.backend.common.enums.Language;
 import kr.codenova.backend.single.entity.TypingSpeed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface TypingSpeedRepository extends JpaRepository<TypingSpeed, Integer> {
 
-    Optional<TypingSpeed> findByMemberIdAndLanguage(Integer memberId, Language language);
+    @Query("SELECT t FROM TypingSpeed t WHERE t.memberId = :memberId AND t.language = :language")
+    Optional<TypingSpeed> findByMemberIdAndLanguage(@Param("memberId") Integer memberId, @Param("language") Language language);
 }
