3aaf76c41e109f20213f5fe2cad0ba7144911b9e
Yujiin <jiin4083@naver.com>
2025-05-04 15:48:33 +0900

    S12P31E103-137 [FE] feat: private routes, 회원가입 제한

---
+++ Front/codenova/src/routes/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+// components/PrivateRoute.jsx
+import { Navigate } from "react-router-dom";
+import useAuthStore from "../store/authStore";
+
+const PrivateRoute = ({ children }) => {
+  const token = useAuthStore((state) => state.token);
+  console.log(token)
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
