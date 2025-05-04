6f653ca0d7c603db9d87d6b7a134b2441ac10af7
jeong young han <daum4572@naver.com>
2025-05-04 14:10:31 +0900

    S12P31E103-149 [BE] feat : 마이페이지 조회 시, 전화번호 및 언어 별 점수 조회 기능 구현

--- Backend/src/main/java/kr/codenova/backend/member/controller/MemberController.java
+++ Backend/src/main/java/kr/codenova/backend/member/controller/MemberController.java
@@ -2,22 +2,31 @@ package kr.codenova.backend.member.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import kr.codenova.backend.common.enums.Language;
+import kr.codenova.backend.global.exception.CustomException;
 import kr.codenova.backend.global.response.ResponseCode;
 import kr.codenova.backend.member.auth.CustomMemberDetails;
 import kr.codenova.backend.member.dto.GuestLoginDto;
+import kr.codenova.backend.member.dto.MemberPhoneNumDto;
 import kr.codenova.backend.member.dto.SignupDto;
 import kr.codenova.backend.member.dto.MemberProfileDto;
 import kr.codenova.backend.member.entity.Member;
 import kr.codenova.backend.global.response.Response;
 import kr.codenova.backend.member.repository.MemberRepository;
 import kr.codenova.backend.member.service.MemberService;
+import kr.codenova.backend.member.util.CustomResponseUtil;
+import kr.codenova.backend.single.entity.TypingSpeed;
+import kr.codenova.backend.single.repository.TypingSpeedRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static kr.codenova.backend.global.response.ResponseCode.*;
 
@@ -28,6 +37,9 @@ public class MemberController {
 
     private final MemberService memberService;
     private final MemberRepository memberRepository;
+    private final TypingSpeedRepository typingSpeedRepository;
+    private static final Logger log = LoggerFactory.getLogger(CustomResponseUtil.class);
+
 
     // 일반 사용자 회원가입
     @PostMapping("/signup")
@@ -43,8 +55,35 @@ public class MemberController {
         Member member = memberRepository.findByIdColumn(id).orElseThrow(
                 () -> new IllegalArgumentException("존재하지 않는 회원입니다: " + id)
         );
-        MemberProfileDto responseDto = new MemberProfileDto(member);
+
+        // 각 언어별 TypingSpeed 가져오기
+        // null 이면 0점으로 처리하기
+        List<TypingSpeed> typingSpeeds = typingSpeedRepository.findAllByMemberId(member.getMemberId());
+        Map<Language, Double> scoreMap = typingSpeeds.stream()
+                .collect(Collectors.toMap(TypingSpeed::getLanguage, TypingSpeed::getTypingSpeed));
+
+        List<MemberProfileDto.UserScore> scoreList = new ArrayList<>();
+        for (String langStr : Language.toLanguageList()) {
+            Language lang = Language.valueOf(langStr);
+            scoreList.add(new MemberProfileDto.UserScore(lang, scoreMap.getOrDefault(lang, 0.0)));
+        }
+
+        MemberProfileDto responseDto = new MemberProfileDto(member, scoreList);
         return new ResponseEntity<>(Response.create(GET_USER_PROFILE, responseDto), GET_USER_PROFILE.getHttpStatus());
+    }
+
+    @PostMapping("/profile")
+    public ResponseEntity<?> updatePhoneNum(@AuthenticationPrincipal CustomMemberDetails memberDetails, @RequestBody MemberPhoneNumDto dto) {
+        Member findMember = memberRepository.findByIdColumn(memberDetails.getMember().getId())
+                .orElseThrow(() -> new CustomException(NOT_FOUND_USER));
+
+        log.info(dto.getPhoneNum());
+        findMember.changePhoneNum(dto.getPhoneNum());
+
+        memberRepository.save(findMember);
+        return new ResponseEntity<>(
+                Response.create(ResponseCode.SUCCESS_CHANGE_PROFILE, findMember.getPhoneNum()), SUCCESS_CHANGE_PROFILE.getHttpStatus()
+        );
     }
 
     @GetMapping("/check-id/{id}")
