3e7438921d69747e60ee156326cc86ba61cb751c
zeeeyeon <zeeyeonleee@gmail.com>
2025-05-07 14:44:35 +0900

    S12P31E103-177 [INFRA] feat: actuator, Spring SecurityConfig permitAll 설정

--- Backend/src/main/java/kr/codenova/backend/global/config/security/SecurityConfig.java
+++ Backend/src/main/java/kr/codenova/backend/global/config/security/SecurityConfig.java
@@ -74,7 +74,8 @@ public class SecurityConfig {
                                     "/api/member/check-nickname/**",
                                     "/api/member/guest",
                                     "/api/single/**",
-                                    "api/single/ranking/**")
+                                    "api/single/ranking/**",
+                                    "/actuator/**")
                             .permitAll()
                             .anyRequest().authenticated();
 
