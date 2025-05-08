cbae8c2844e5d7f2927512e16854ebfe15177f7d
skdud5126 <skdud5126@naver.com>
2025-05-08 13:30:10 +0900

    S12P31E103-190 [FE] feat : 멀티모드 3라운드 종료시 대기방으로 이동 기능 구현 완료

--- Front/codenova/src/components/multi/modal/FinalResultModal.jsx
+++ Front/codenova/src/components/multi/modal/FinalResultModal.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const FinalResultModal = ({ visible, results = [], onClose }) => {
+const FinalResultModal = ({ visible, results = [], onClose, roomInfo }) => {
+
+    console.log("➡️ 이동 시도 roomInfo:", roomInfo);
+    const navigate = useNavigate();
   if (!visible) return null;
+
+  const handleConfirm = () => {
+    // 대기방으로 이동하면서 최신 roomInfo도 같이 넘김
+    navigate(`/multi/room/${String(roomInfo.roomId)}`, {
+      state: { roomInfo }, // 공개방 or 비공개방 여부 포함해서 넘김
+    });
+    onClose(); // 모달 닫기
+  };
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
@@ -18,10 +30,10 @@ const FinalResultModal = ({ visible, results = [], onClose }) => {
         </ul>
 
         <button
-          onClick={onClose}
+          onClick={handleConfirm}
           className="mt-6 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
         >
-          확인
+          대기방으로 이동!
         </button>
       </div>
     </div>
