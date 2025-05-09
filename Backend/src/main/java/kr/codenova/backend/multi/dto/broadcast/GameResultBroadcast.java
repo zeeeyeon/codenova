93f9d1897732e00c064d0358a7843026fab8a7a8
jeong young han <daum4572@naver.com>
2025-05-09 15:11:46 +0900

    S12P31E103-168 [BE] refactor: 라운드 별 점수 관리 로직 수정

--- Backend/src/main/java/kr/codenova/backend/multi/dto/broadcast/GameResultBroadcast.java
+++ Backend/src/main/java/kr/codenova/backend/multi/dto/broadcast/GameResultBroadcast.java
@@ -21,7 +21,7 @@ public class GameResultBroadcast {
     @Builder
     public static class UserResultStatus {
         private String nickname;
-        private Double averageScore;
+        private Integer totalScore;
         private int rank;
     }
 }
