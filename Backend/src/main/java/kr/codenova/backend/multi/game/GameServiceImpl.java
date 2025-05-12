34a9798ee5a49aa902657a5d0b6c828e955a51d4
jeong young han <daum4572@naver.com>
2025-05-12 11:59:11 +0900

    S12P31E103-199 [BE] refactor: 라운드 종료 시 점수 수정

--- Backend/src/main/java/kr/codenova/backend/multi/game/GameServiceImpl.java
+++ Backend/src/main/java/kr/codenova/backend/multi/game/GameServiceImpl.java
@@ -223,14 +223,14 @@ public class GameServiceImpl implements GameService {
 
             calculateScores(room);
 
+            RoundScoreBroadcast broadcast = buildRoundScoreBroadcast(room);
+            getServer().getRoomOperations(roomId)
+                    .sendEvent("round_score", broadcast);
             // ✅ 라운드 수에 따라 자동 종료 또는 다음 라운드
             int MAX_ROUND = 3;
             if (room.getRoundNumber() >= MAX_ROUND) {
                 endGame(roomId); // 🎯 자동 게임 종료
             } else {
-                RoundScoreBroadcast broadcast = buildRoundScoreBroadcast(room);
-                getServer().getRoomOperations(roomId)
-                        .sendEvent("round_score", broadcast);
                 resetRoundData(room);
             }
         }
