e8b89f46086c57aa2cfc180abdbae5370f837360
이지연 <jiyeon_0342@naver.com>
2025-05-12 14:54:58 +0900

    S12P31E103-214 [BE] feat: 모니터링, 로깅 시스템 구축 (회원, 비회원 회원가입 로깅)

--- Backend/src/main/java/kr/codenova/backend/single/controller/CodeResultController.java
+++ Backend/src/main/java/kr/codenova/backend/single/controller/CodeResultController.java
@@ -1,11 +1,13 @@
 package kr.codenova.backend.single.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import kr.codenova.backend.global.response.Response;
 import kr.codenova.backend.member.auth.CustomMemberDetails;
 import kr.codenova.backend.single.dto.VerifyResponseDto;
 import kr.codenova.backend.single.dto.request.CodeResultRequest;
 import kr.codenova.backend.single.service.impl.CodeResultService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static kr.codenova.backend.global.response.ResponseCode.CODE_RESULT_SUCCESS;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/single/code")
 @RequiredArgsConstructor
@@ -23,9 +26,18 @@ public class CodeResultController {
     private final CodeResultService codeResultService;
 
     @PostMapping("/verify")
-    public ResponseEntity<?> verifyCodeResult(@AuthenticationPrincipal CustomMemberDetails memberDetails, @RequestBody CodeResultRequest request) {
+    public ResponseEntity<?> verifyCodeResult(@AuthenticationPrincipal CustomMemberDetails memberDetails, @RequestBody CodeResultRequest request, HttpServletRequest httpServletRequest) {
         Integer memberId = (memberDetails != null) ? memberDetails.getMember().getMemberId() : null;
         VerifyResponseDto response = codeResultService.verifyAndGenerateToken(request, memberId);
+
+        String userType = "회원";
+
+
+        log.info("게임_완료,유형:{},ID:{},닉네임:{},모드:{},언어:{},WPM:{},소요시간:{}ms,IP:{}",
+                userType, memberId, memberDetails.getMember().getNickname(), "single", request.language(),
+                response.typingSpeed(), httpServletRequest.getRemoteAddr());
+
+
         return new ResponseEntity<>(Response.create(CODE_RESULT_SUCCESS, response), CODE_RESULT_SUCCESS.getHttpStatus());
     }
 }
