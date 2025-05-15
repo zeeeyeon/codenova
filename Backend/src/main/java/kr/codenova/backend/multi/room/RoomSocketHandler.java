3fc4d0e465301c5c516036600c8628220e118f25
jeong young han <daum4572@naver.com>
2025-05-15 10:04:34 +0900

    S12P31E103-239 [BE] refactor: log 객체 중복 생성 문제 수정

--- Backend/src/main/java/kr/codenova/backend/multi/room/RoomSocketHandler.java
+++ Backend/src/main/java/kr/codenova/backend/multi/room/RoomSocketHandler.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RoomSocketHandler implements SocketEventHandler {
