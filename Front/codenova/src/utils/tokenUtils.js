8ac78d71374ffe5630654db7a219678c38566f45
송동현 <dustkscjswo@naver.com>
2025-05-16 15:24:30 +0900

    Revert "Merge branch 'fe/feat/apiencry' into 'master'"

--- Front/codenova/src/utils/tokenUtils.js
+++
@@ -1,9 +0,0 @@
-// utils/tokenUtils.js
-export const getAccessToken = () => {
-  try {
-    const data = JSON.parse(localStorage.getItem("auth-storage"));
-    return data?.state?.token || null;
-  } catch (e) {
-    return null;
-  }
-};
